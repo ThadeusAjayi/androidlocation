c8eb923c8a0fd958ba201d79fe6ec46453ce121d
Thadeus Ajayi <ainojie@gmail.com>
2018-05-25 13:32:18 +0100

    Changes to location service detected in receiver

---
+++ app/src/main/java/shopspreeng/locationtest/LocationReciever.java
@@ -0,0 +1,25 @@
+package shopspreeng.locationtest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
+
+/**
+ * Created by Thadeus-APMIS on 5/25/2018.
+ */
+
+public class LocationReciever extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        if (intent.getAction().matches("android.location.PROVIDERS_CHANGED")) {
+            Toast.makeText(context, "in android.location.PROVIDERS_CHANGED",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+}
--- app/src/main/java/shopspreeng/locationtest/MainActivity.java
+++ app/src/main/java/shopspreeng/locationtest/MainActivity.java
@@ -1,11 +1,22 @@
 package shopspreeng.locationtest;
 
+import android.*;
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
@@ -26,6 +37,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     TextView messageTextView;
     GoogleApiClient googleApiClient;
     LocationRequest locationRequest;
+    LocationReciever locationReciever;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,13 +57,18 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
     @Override
     protected void onStart() {
+        registerReceiver(locationReciever, new IntentFilter(LocationManager.PROVIDERS_CHANGED_ACTION));
         googleApiClient.connect();
         super.onStart();
     }
 
     @Override
     protected void onStop() {
-        googleApiClient.disconnect();
+        if (locationReciever != null) {
+           unregisterReceiver(locationReciever);
+        }
+        if (googleApiClient.isConnected())
+            googleApiClient.disconnect();
         super.onStop();
     }
 
@@ -88,4 +105,49 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     public void onLocationChanged(Location location) {
         messageTextView.setText("Latitude: " + location.getLatitude() + "\n" + "Longitude: " + location.getLongitude());
     }
+
+    void requestPermissions () {
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        234);
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 234: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, Disable the
+                    // functionality that depends on this permission.
+                    //TODO show dialog and on yes request permission again on no cancel request permission
+                    requestPermissions();
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this application might request
+        }
+    }
+
 }
